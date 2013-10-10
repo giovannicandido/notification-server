64fdffb82180d3f4ec4df93d19c23daf14cfc690
Giovanni Silva <giovanni@atende.info>
2013-10-10 13:57:05 -0300

    Gradle com Arquillian, Intellij IDEA e Java EE 8

---
+++ src/main/java/MySimpleBeam.java
@@ -0,0 +1,13 @@
+import javax.inject.Named;
+
+/**
+ * Criado por Giovanni Silva <giovanni@atende.info>
+ * Date: 10/9/13
+ * Time: 11:32 PM
+ */
+@Named
+public class MySimpleBeam {
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
