43c8e76271de82de4501b6668a789c9bce2b988f
Giovanni Silva <giovanni@pucminas.br>
2016-03-28 16:55:30 -0300

    #4 Client clean up token page router

--- src/main/webapp/src/app.ts
+++ src/main/webapp/src/app.ts
@@ -1,14 +1,14 @@
 import {Router, RouterConfiguration} from 'aurelia-router'
 
 export class App {
-  router: Router;
-  
-  configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+  router:Router;
+
+  configureRouter(config:RouterConfiguration, router:Router) {
+    config.title = 'NF Server';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      {route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Notification Server'},
+      {route: 'token', name: 'token', moduleId: 'token', nav: true, title: 'Tokens'},
+      {route: 'graph', name: 'graph', moduleId: 'graphs', nav: true, title: 'Graphs'}
     ]);
 
     this.router = router;
