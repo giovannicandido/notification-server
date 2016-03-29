ccf0007f6cb111a78c7bc9e313ffe68f108e3aa0
Giovanni Silva <giovanni@pucminas.br>
2016-03-28 21:08:19 -0300

    #3 Login with keycloak functional. Documentation about send email

--- docs/sending-email.md
+++ docs/sending-email.md
@@ -1,3 +1,13 @@
 # Sending Email from Other Applications
 
-Generate a token for the application in
+Generate a token for the application in the application Admin Page (the default page)
+
+Send a **POST** to the application url: **/api/notification/email** with the contents:
+
+| Request Param | Description                                                    | Constraints           |
+|---------------|----------------------------------------------------------------|-----------------------|
+| token         | Token for security                                             | Required              |
+| email         | Destination Email, multiples emails are separated by comma (;) | Required. Valid email |
+| subject       | Subject of the message                                         | Required              |
+| html          | If the email is Html content                                   | Option                |
+
---
+++ src/main/java/info/atende/nserver/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package info.atende.nserver.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ * Configuração de segurança da aplicação
+ * @author Giovanni Silva.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+{
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.csrf().ignoringAntMatchers("/api/notification/email");
+        http
+                .authorizeRequests()
+                .antMatchers("/api/notification/email")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/logout")
+                .and().exceptionHandling();
+//                .antMatchers("/admin*").hasRole("ADMIN")
+
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+            KeycloakAuthenticationProcessingFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    @ConditionalOnClass(SpringBootApplication.class)
+    public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+            KeycloakPreAuthActionsFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+}
