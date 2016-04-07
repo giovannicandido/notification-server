e91bcbbd168be51a3d838028acf7b3fd4706c0fd
Giovanni Silva <giovanni@pucminas.br>
2016-04-07 13:12:59 -0300

    chore: Linhas no teste TestNotificationService

--- src/test/java/info/atende/nserver/TestNotificationServiceUnit.java
+++ src/test/java/info/atende/nserver/TestNotificationServiceUnit.java
@@ -19,11 +19,15 @@ public class TestNotificationServiceUnit {
     @Test
     public void testSendEmailWithInvalidToken() throws EmailNotSendedException {
         Notification notification = mock(Notification.class);
+
         when(notification.validateToken(anyObject())).thenReturn(false);
+
         NotificationService notificationService = new NotificationService(notification);
         HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+
         ResponseEntity<String> stringResponseEntity = notificationService.sendEmail("email@email.com", "subject", "message", null, true, "invalid",
                 httpServletRequest);
+
         assertEquals("Response should be Bad Request", HttpStatus.BAD_REQUEST, stringResponseEntity.getStatusCode());
         assertEquals("Response Text should be invalid token", "invalid token", stringResponseEntity.getBody());
     }
@@ -31,12 +35,16 @@ public class TestNotificationServiceUnit {
     @Test
     public void testWithValidToken() throws EmailNotSendedException {
         Notification notification = mock(Notification.class);
+
         when(notification.validateToken(anyObject())).thenReturn(true);
+
         NotificationService notificationService = new NotificationService(notification);
         HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
         String[] to = {"email@email.com"};
+
         notificationService.sendEmail(to[0], "subject","message", "no-reply@test.com", true, "invalid",
                 httpServletRequest);
+
         verify(notification).sendEmail(eq(to), eq("no-reply@test.com"), eq("subject"),eq("message"), anyObject());
     }
 }
