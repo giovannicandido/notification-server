e39013f30a8d2aa1a69c83a9003bde7b5bbe1aa8
Giovanni Silva <giovanni@atende.info>
2016-09-24 11:26:08 -0300

    #5 client tests with gradle build

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,15 +1,23 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppModule} from './app.module';
+import {AppComponent} from './app.component'
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {APP_BASE_HREF} from "@angular/common";
 
-describe('App: Webapp2', () => {
+
+describe('App: Notification Server', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/'},
+      ],
       declarations: [
         AppComponent
       ],
-    });
+      schemas:      [ NO_ERRORS_SCHEMA ]
+    }).compileComponents();
   });
 
   it('should create the app', async(() => {
@@ -18,16 +26,11 @@ describe('App: Webapp2', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should contain a router-outlet inside div.page-host', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('div.page-host router-outlet')).not.toBeNull();
   }));
 });
